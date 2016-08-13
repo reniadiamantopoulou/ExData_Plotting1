b903c8599a2cbbe45ede90497419b16880f781c0
Renia Diamantopoulou <rdiamant2011@gmail.com>
2016-08-13 17:42:09 +0100

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,15 @@
+################################################################################
+
+#Data Science Specialization, Coursera
+#     Exploratory Data plotting 
+
+# Cretaed by Renia Diamantopoulou
+#14 August 2016
+
+################################################################################
+
+png("plot1.png",width=480,height=480)
+hist(pwr$GlobalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
+source('power_consumption.r')
